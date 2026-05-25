f91ac0d15aee8783fa37d636aed329d1f361f3ca
Bilal-Ahmad102 <ba2736180@gmail.com>
2026-05-25 19:10:02 +0500

    finalize , remove debug

--- Scipts/Globals/NightManager.gd
+++ Scipts/Globals/NightManager.gd
@@ -5,8 +5,7 @@ signal night_ended(night: int, success: bool)
 signal animatronics_updated(active_list: Array)
 
 const NIGHT_ANIMATRONICS: Dictionary = {
-	#1: ["Dave"],
-	1: ["Bronnie"],
+	1: ["Dave"],
 	2: ["Dave", "Frednic", "Rena"],
 	3: ["Dave", "Rena", "Ambassador"],
 	4: ["Dave", "Frednic", "Rena", "Ambassador"],
